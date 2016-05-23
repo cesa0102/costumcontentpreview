831c57e03c99748c25d6ac8c480d902a686336ec
Christian Stern <jetzt-mal-ehrlich@ich-bin-ein-arschloch.de>
2016-05-23 21:01:24 +0200

    Add TSconfig

--- ext_tables.php
+++ ext_tables.php
@@ -10,3 +10,18 @@ $GLOBALS['TBE_STYLES']['skins']['costumcontentpreview'] = array(
         'css' => 'EXT:costumcontentpreview/Resources/Public/Css/typo3_backend/'
     )
 );
+
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+mod {
+    web_layout {
+        tt_content {
+            preview {
+                html = EXT:costumcontentpreview/Resources/Private/Templates/Html.html
+                bullets = EXT:costumcontentpreview/Resources/Private/Templates/Bullets.html
+                table = EXT:costumcontentpreview/Resources/Private/Templates/Table.html
+            }
+        }
+    }
+}
+');
