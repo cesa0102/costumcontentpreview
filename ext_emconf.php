4f0f1e75b0aed2a0543257c9a995442328893f8a
Christian Stern <jetzt-mal-ehrlich@ich-bin-ein-arschloch.de>
2016-05-24 01:05:16 +0200

    Changed depends version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.1.3',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.9.99'
+			'typo3' => '7.6.0-7.9.99'
 		),
 		'conflicts' => array(
 		),
