7290dc2dbb48df9b3e0ef9cd57449fd56903edcf
Christian Stern <jetzt-mal-ehrlich@ich-bin-ein-arschloch.de>
2016-05-25 15:37:12 +0200

    Formated sourcecode and changed version-number

--- Classes/ViewHelpers/CroppedPreviewOfContentViewHelper.php
+++ Classes/ViewHelpers/CroppedPreviewOfContentViewHelper.php
@@ -9,114 +9,117 @@ use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
  *
  * @package Mdy\Costumcontentpreview\ViewHelpers
  */
-class CroppedPreviewOfContentViewHelper extends AbstractViewHelper {
+class CroppedPreviewOfContentViewHelper extends AbstractViewHelper
+{
 
     /**
      * Arguments Initialization
      */
-    public function initializeArguments() {
-	$this->registerArgument(
-		'content', 'string', 'content to be used inside viewhelper. E.g. bodytext', TRUE
-	);
+    public function initializeArguments()
+    {
+        $this->registerArgument(
+            'content', 'string', 'content to be used inside viewhelper. E.g. bodytext', TRUE
+        );
 
+        $this->registerArgument(
+            'linesToCrop', 'int', 'amount of lines', FALSE, 3
+        );
 
-	$this->registerArgument(
-		'linesToCrop', 'int', 'amount of lines', FALSE, 3
-	);
+        $this->registerArgument(
+            'explodeRows', 'string', 'string to split content', FALSE
+        );
 
-	$this->registerArgument(
-		'explodeRows', 'string', 'string to split content', FALSE
-	);
+        $this->registerArgument(
+            'explodeItems', 'string', 'string to split lines', FALSE
+        );
 
-	$this->registerArgument(
-		'explodeItems', 'string', 'string to split lines', FALSE
-	);
+        $this->registerArgument(
+            'itemWrap', 'string', 'a wrap that will pear around each item', FALSE
+        );
 
-	$this->registerArgument(
-		'itemWrap', 'string', 'a wrap that will pear around each item', FALSE
-	);
+        $this->registerArgument(
+            'lineWrap', 'string', 'a wrap that will pear around each line', FALSE
+        );
 
-	$this->registerArgument(
-		'lineWrap', 'string', 'a wrap that will pear around each line', FALSE
-	);
-
-	$this->registerArgument(
-		'contentWrap', 'string', 'a wrap that will pear around the complete output', FALSE, "|"
-	);
+        $this->registerArgument(
+            'contentWrap', 'string', 'a wrap that will pear around the complete output', FALSE, "|"
+        );
     }
 
-    private function splitNewLine($text) {
-	$code = preg_replace('/\n$/', '', preg_replace('/^\n/', '', preg_replace('/[\r\n]+/', "\n", $text)));
-	return explode("\n", $code);
+    private function splitNewLine($text)
+    {
+        $code = preg_replace('/\n$/', '', preg_replace('/^\n/', '', preg_replace('/[\r\n]+/', "\n", $text)));
+        return explode("\n", $code);
     }
 
     /**
      * @return string $output
      */
-    public function render() {
-	$content = $this->arguments['content'];
-	$linesToCrop = intval($this->arguments['linesToCrop']);
-	$explodeRows = $this->arguments['explodeRows'];
-	$explodeItems = $this->arguments['explodeItems'];
-	$itemWrap = $this->arguments['itemWrap'];
-	$lineWrap = $this->arguments['lineWrap'];
-	$contentWrap = $this->arguments['contentWrap'];
+    public function render()
+    {
+        $content = $this->arguments['content'];
+        $linesToCrop = intval($this->arguments['linesToCrop']);
+        $explodeRows = $this->arguments['explodeRows'];
+        $explodeItems = $this->arguments['explodeItems'];
+        $itemWrap = $this->arguments['itemWrap'];
+        $lineWrap = $this->arguments['lineWrap'];
+        $contentWrap = $this->arguments['contentWrap'];
+        $output = "";
 
-	$cObject = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer');
+        $cObject = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer');
 
 
-	if ($content && $linesToCrop > 0) {
-	    if ($explodeRows) {
-		$contentLines = explode($explodeRows, $content);
-	    } else {
-		$contentLines = $this->splitNewLine($content);
-	    }
+        if ($content && $linesToCrop > 0) {
+            if ($explodeRows) {
+                $contentLines = explode($explodeRows, $content);
+            } else {
+                $contentLines = $this->splitNewLine($content);
+            }
 
-	    $contentAmoutOfLines = count($contentLines);
-	    $croppedContentLines = array_splice($contentLines, 0, $linesToCrop);
+            $contentAmoutOfLines = count($contentLines);
+            $croppedContentLines = array_splice($contentLines, 0, $linesToCrop);
 
-	    foreach ($croppedContentLines as $singleLine) {
-		if ($explodeItems) {
-		    $contentRowItems = explode($explodeItems, $singleLine);
-		    $newSingleLine = "";
-		    foreach ($contentRowItems as $singleItem) {
-			$newSingleLine .= $cObject->wrap($singleItem, $itemWrap);
-		    }
+            foreach ($croppedContentLines as $singleLine) {
+                if ($explodeItems) {
+                    $contentRowItems = explode($explodeItems, $singleLine);
+                    $newSingleLine = "";
+                    foreach ($contentRowItems as $singleItem) {
+                        $newSingleLine .= $cObject->wrap($singleItem, $itemWrap);
+                    }
 
-		    if ($lineWrap) {
-			$output .= $cObject->wrap($newSingleLine, $lineWrap);
-		    } else {
-			$output .= $singleLine . "\n";
-		    }
-		} elseif (strpos($singleLine, '|') !== false && !$explodeItems) {
-		    $contentWrap = '<dl class="ccpDlWrap">|</dl>';
-		    $contentRowItems = explode("|", $singleLine);
-		    $newSingleLine = "";
-		    foreach ($contentRowItems as $i => $singleItem) {
-			if ($i % 2 == 0) {
-			    $output .= $cObject->wrap($singleItem, "<dt>|</dt>");			  
-			}
-			else {	
-			    $output .= $cObject->wrap($singleItem, "<dd>|</dd>");			  			    
-			}
-		    }
-		} else {
-		    if ($lineWrap) {
-			$output .= $cObject->wrap($singleLine, $lineWrap);
-		    } else {
-			$output .= $singleLine . "\n";
-		    }
-		}
-	    }
+                    if ($lineWrap) {
+                        $output .= $cObject->wrap($newSingleLine, $lineWrap);
+                    } else {
+                        $output .= $singleLine . "\n";
+                    }
+                } elseif (strpos($singleLine, '|') !== false && !$explodeItems) {
+                    $contentWrap = '<dl class="ccpDlWrap">|</dl>';
+                    $contentRowItems = explode("|", $singleLine);
+                    $newSingleLine = "";
+                    foreach ($contentRowItems as $i => $singleItem) {
+                        if ($i % 2 == 0) {
+                            $output .= $cObject->wrap($singleItem, "<dt>|</dt>");
+                        } else {
+                            $output .= $cObject->wrap($singleItem, "<dd>|</dd>");
+                        }
+                    }
+                } else {
+                    if ($lineWrap) {
+                        $output .= $cObject->wrap($singleLine, $lineWrap);
+                    } else {
+                        $output .= $singleLine . "\n";
+                    }
+                }
+            }
 
-	    $output = $cObject->wrap($output, $contentWrap);
+            $output = $cObject->wrap($output, $contentWrap);
 
-	    if ($contentAmoutOfLines > $linesToCrop) {
-		$output .= "...";
-	    }
-	}
+            if ($contentAmoutOfLines > $linesToCrop) {
+                $output .= "...";
+            }
+        }
 
-	return $output;
+        return $output;
     }
 
 }
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,30 +1,28 @@
 <?php
 
 /***************************************************************
- * 
+ *
  * Extension Manager/Repository config file for ext: "costumcontentpreview"
- * 
+ *
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Cropped and better visibile content elements',
-	'description' => 'Adds a better preview for the elements: html, table and bullets.',
-	'category' => 'plugin',
-	'author' => 'Christian Stern',
-	'author_email' => 'christian.stern@pornofilm-produzent.de',
-	'state' => 'beta',
-	'internal' => '',
-	'uploadfolder' => '0',
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'version' => '0.1.4',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '7.6.0-7.9.99'
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
+    'title' => 'Cropped and better visibile content elements',
+    'description' => 'Adds a better preview for the elements: html, table and bullets.',
+    'category' => 'plugin',
+    'author' => 'Christian Stern',
+    'author_email' => 'christian.stern@pornofilm-produzent.de',
+    'state' => 'beta',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.1.5',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.0-7.9.99'
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
 );
