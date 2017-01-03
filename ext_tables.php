12b9a1655f6b892cc69bb0fbff1c9063ffff9c7d
Christian Stern <jetzt-mal-ehrlich@ich-bin-ein-arschloch.de>
2017-01-03 09:59:17 +0100

    Tested extension in typo3 8.5. Changed state and version number.

--- ext_tables.php
+++ ext_tables.php
@@ -5,13 +5,16 @@ if (!defined('TYPO3_MODE')) {
 }
 
 $GLOBALS
-    ['TYPO3_CONF_VARS']
-    ['SC_OPTIONS']
-    ['cms/layout/class.tx_cms_layout.php']
-    ['tt_content_drawItem']
-    ['costumcontentpreview'] = 'EXT:costumcontentpreview/Classes/Hooks/PageLayoutView.php:Mdy\costumcontentpreview\Hooks\PageLayoutView';
+['TYPO3_CONF_VARS']
+['SC_OPTIONS']
+['cms/layout/class.tx_cms_layout.php']
+['tt_content_drawItem']
+['costumcontentpreview'] = 'EXT:costumcontentpreview/Classes/Hooks/PageLayoutView.php:Mdy\costumcontentpreview\Hooks\PageLayoutView';
 
-$GLOBALS['TBE_STYLES']['skins']['costumcontentpreview'] = array(
+$GLOBALS
+['TBE_STYLES']
+['skins']
+['costumcontentpreview'] = array(
     'name' => 'costumcontentpreview',
     'stylesheetDirectories' => array(
         'css' => 'EXT:costumcontentpreview/Resources/Public/Css/typo3_backend/'
